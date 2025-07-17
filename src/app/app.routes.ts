8a8c55db50bf6203707729eecdb79ed45a054722
audrey <audreym.sturdy@gmail.com>
2025-07-17 22:38:56 +0200

    created interface ,setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { View } from './post/view/view';
+import { Edit } from './post/edit/edit';
+import { Create } from './post/create/create';
+import { Index } from "./post/index";
 
-export const routes: Routes = [];
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: Index,
+    title: 'Posts'
+  },
+  {
+    path: 'post/create',
+    component: Create,
+    title: 'Create Post'
+  },
+  {
+    path: 'post/:id/edit',
+    component: Edit,
+    title: 'Edit Post'
+  },
+  {
+    path: 'post/:id/view',
+    component: View,
+    title: 'View Post'
+  }
+];
